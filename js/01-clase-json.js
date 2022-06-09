4f7dc3e965af2a8b0e9553629f8da74c368963ee
Diego Davila <diego.davilafreitas@gmail.com>
2022-06-09 23:16:36 +0200

    add ts

--- js/01-clase-json.js
+++ js/01-clase-json.js
@@ -1,3 +1,12 @@
 
-
-
+var bicicleta = {
+    color: 'rojo',
+    modelo: 'bmx',
+    frenos: 'disco',
+    velocidadMaxima: '60km/h',
+    cambiaColor : function(nuevoColor){
+        //bicicleta.color(nuevoColor)
+        this.color = nuevoColor
+    }
+};
+console.log(bicicleta)
