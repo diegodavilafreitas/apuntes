b8ec32060408afcb635a5442ee9f46f5fb6fe246
Diego Davila <diego.davilafreitas@gmail.com>
2022-06-23 19:29:13 +0200

    modelo1.0

---
+++ apuntesAngular/aprendiendoAngular/src/app/Models/configuracion.ts
@@ -0,0 +1,7 @@
+export var Configuración = ({
+    color:'red',
+    fondo: 'blue',
+    titulo: 'Aplicación con Angular',
+    descripcion: 'Esto es una aplicación desarrollada en Angular'
+});
+
--- apuntesAngular/aprendiendoAngular/src/app/app.component.ts
+++ apuntesAngular/aprendiendoAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Configuración } from './Models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public title: String ='';
+  public description: String='';
+  public colorTitulos: String='';
   name = 'Diego';
   scndName = 'Davila Freitas';
   mostrarVideojuego: boolean = true;
 
-  ocultarVideojuego(){
-    this.mostrarVideojuego = false;
+  constructor(){
+    this.title = Configuración.titulo;
+    this.description = Configuración.descripcion;
+    this.colorTitulos = Configuración.color;
   }
+
+  ocultarVideojuego(estado: boolean){
+    this.mostrarVideojuego = estado;
+  }
+
+  
 
 }
