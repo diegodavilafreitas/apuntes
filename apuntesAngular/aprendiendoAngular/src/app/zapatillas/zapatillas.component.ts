67fa86aa65b6c9cbe7710a13efdbc9134af7918b
Diego Davila <diego.davilafreitas@gmail.com>
2022-06-25 20:49:03 +0200

    directivas if y for

--- apuntesAngular/aprendiendoAngular/src/app/zapatillas/zapatillas.component.ts
+++ apuntesAngular/aprendiendoAngular/src/app/zapatillas/zapatillas.component.ts
@@ -9,18 +9,33 @@ export class ZapatillasComponent implements OnInit{
 
     public titulo: String ='Componente zapatillas';
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
     constructor(){
+        this.marcas = new Array;
         this.zapatillas = [
             new Zapatilla('Reebok classic', 100, 'rojo', 'reebok', true),
             new Zapatilla('Reebok new', 110, 'azul', 'reebok', true),
             new Zapatilla('nike air', 150, 'blanco', 'nike', false),
-            new Zapatilla('adidas cortez', 90, 'rojo, azul y blanco', 'adidas', true)
+            new Zapatilla('adidas cortez', 90, 'rojo, azul y blanco', 'adidas', true),
+            new Zapatilla('adidas loko', 50, 'blanco', 'adidas', false),
         ];
     }
 
     ngOnInit(): void {
         console.log(this.zapatillas)
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach(element => {
+            if(!this.marcas.includes(element.marca)){
+                this.marcas.push(element.marca);
+            }
+            
+        });
+        console.log(this.marcas)
+
     }
 
 
